3a09c4c1a78e401d47930cc42ca40e25eb8608bf
Gabriel Cavalieri <cavalieri2005@hotmail.com>
2025-04-14 21:50:14 -0400

    tividades

---
+++ 14-04-25/ex1.py
@@ -0,0 +1,14 @@
+nota1 = float(input("Informe a primeira nota: "))
+nota2 = float(input("Informe a segunda nota: "))
+
+media = (nota1 + nota2) / 2
+
+if media >= 7 and media <= 9.999999999999999999999999:{
+    print("Aprovado")
+}
+elif media < 7:{
+    print("Reprovado")
+}
+elif media == 10:{
+    print("Aprovado com Distinção")
+}

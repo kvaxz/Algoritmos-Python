a1ab7e5527d05a42209b2acf7f333c2622194a00
Gabriel Cavalieri <cavalieri2005@hotmail.com>
2025-08-29 19:29:59 -0400

    L

---
+++ POO-THIAGO/exercicio.py
@@ -0,0 +1,3 @@
+nome = "otorrinolaringologista"
+
+print(len(nome))
---
+++ POO-THIAGO/func.py
@@ -0,0 +1,11 @@
+def soma(n1,n2):
+    res = n1 + n2
+    return res
+
+print(soma(3,3))
+
+def eleva(num1,num2):
+    result = num1**num2
+    return result
+
+print(eleva(2,3))
